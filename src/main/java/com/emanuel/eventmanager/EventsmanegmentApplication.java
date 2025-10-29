57b25491dac4eeae6c6666eaab943d3af0419e76
itoshi-m-dev <emanuelkalison@gmail.com>
2025-10-29 17:02:27 -0300

    Inicio do projeto

---
+++ src/main/java/com/emanuel/eventmanager/EventsmanegmentApplication.java
@@ -0,0 +1,13 @@
+package com.emanuel.eventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsmanegmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsmanegmentApplication.class, args);
+	}
+
+}
